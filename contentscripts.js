24b639ce000389350913fa3ba433cf8635e835c1
TonyZhang1002 <15205944@ucdconnect.ie>
2017-11-22 16:44:31 +0800

    blind the account and password

--- contentscripts.js
+++ contentscripts.js
@@ -1,8 +1,8 @@
  //填写默认账号密码并登录，账号密码可以自己更改 
 chrome.extension.sendRequest({}, function(response) { 
       
-      	$(document).xpathEvaluate('/html/body/div/div/div[2]/form/table/tbody/tr[5]/td[2]/input').val("15372233");
-      	$(document).xpathEvaluate('/html/body/div/div/div[2]/form/table/tbody/tr[6]/td[2]/input').val("jk123456");
+      	$(document).xpathEvaluate('/html/body/div/div/div[2]/form/table/tbody/tr[5]/td[2]/input').val("xxxxx");
+      	$(document).xpathEvaluate('/html/body/div/div/div[2]/form/table/tbody/tr[6]/td[2]/input').val("xxxxx");
       	$(document).xpathEvaluate('/html/body/div/div/div[2]/form/table/tbody/tr[7]/td/p/input[1]').attr("checked","checked");
         $(document).xpathEvaluate('/html/body/div/div/div[2]/form/table/tbody/tr[9]/td[2]/input[1]').click();
 }); 
